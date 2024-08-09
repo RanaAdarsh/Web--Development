0945573a97c0072fc4f642413f1603a03dc7c49e
Adarsh-Rana <92674762+Adarsh-Rana@users.noreply.github.com>
2024-08-09 20:08:42 +0530

    Adding js file.

---
+++ JavaScript/4InbuiltFunc.js
@@ -0,0 +1,15 @@
+//SetTimout Function
+console.log("Hello ,there");
+setTimeout(()=>{
+    console.log("Hello again!")
+},4000)
+
+console.log("hello , ho gya kya ?");
+
+
+//SetInterval Function
+console.log("Interval check kr");
+// setInterval(()=>{
+//     console.log("chal mere bhai")
+// },2000);
+console.log("Chal rha h ki ni ?")
